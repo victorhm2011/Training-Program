c2c99acce4c5a6a6a5b4ca80d54e69799eb3787e
victor <victorhm2011@gmail.com>
2020-07-12 22:50:13 -0400

    Add components repositories and repository-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { UserItemComponent } from './components/user-item/user-item.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
+import { RepositoryItemComponent } from './components/repository-item/repository-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserItemComponent
+    UserItemComponent,
+    RepositoriesComponent,
+    RepositoryItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/repository-item/repository-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repository-item',
+  templateUrl: './repository-item.component.html',
+  styleUrls: ['./repository-item.component.css']
+})
+export class RepositoryItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
